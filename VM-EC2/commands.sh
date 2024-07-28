c657a70de093172e5488e2c01d6cbda8da5a4acf
Ghasemi Farinaz <farinaz.ghasemi@acast.com>
2024-07-28 19:17:06 +0200

    vm, vpc, sg, keypair, public-ip-IGW, nic=ENI disk=volume

---
+++ VM-EC2/commands.sh
@@ -0,0 +1,17 @@
+az vm show --resource-group LunaResourceGroup --name luna-vm
+az network nsg show --resource-group LunaResourceGroup --name luna-vm-nsg
+az network nsg show --resource-group LunaResourceGroup --name lunasg
+az network nsg show --resource-group LunaResourceGroup --name lunavmnsg880
+az network nsg show --resource-group LunaResourceGroup --name ssh
+az network public-ip show --resource-group LunaResourceGroup --name LunaIGW
+az network public-ip show --resource-group LunaResourceGroup --name luna-vm-ip
+az network vnet show --resource-group LunaResourceGroup --name luna-vnet
+az disk show --resource-group LunaResourceGroup --name luna-vm_OsDisk_1_e22f802b5c7f47de936833e07af12cd9
+az sshkey show --resource-group LunaResourceGroup --name luna-vm_key
+az network nic show --resource-group LunaResourceGroup --name luna-vm923_z2
+
+
+
+
+
+
